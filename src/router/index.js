1e7f41c86286b6b4ca4b75cbee7a8d19b409298c
jie.cao <jie.cao@ersoft.cn>
2022-01-28 16:16:51 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -6,24 +6,29 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    redirect: "/zte/e8820v2"
+    redirect: "/index"
   },
   {
     path: '/e8820v2',
-    redirect: "/zte/e8820v2"
+    redirect: "/zte/eeprom/e8820v2"
   },
   {
-    path: '/zte/e8820v2',
+    path: '/index',
+    name: 'Index',
+    component: () => import(/* webpackChunkName: "Index" */ '../views/Index/index.vue')
+  },
+  {
+    path: '/zte/eeprom/e8820v2',
     name: 'E8820V2',
     component: () => import(/* webpackChunkName: "E8820V2" */ '../views/E8820V2/index.vue')
   },
   {
-    path: '/zte/e8820s',
+    path: '/zte/eeprom/e8820s',
     name: 'E8820S',
     component: () => import(/* webpackChunkName: "E8820S" */ '../views/E8820S/index.vue')
   },
   {
-    path: '/tplink/wdr7300',
+    path: '/tplink/eeprom/wdr7300',
     name: 'WDR7300',
     component: () => import(/* webpackChunkName: "WDR7300" */ '../views/WDR7300/index.vue')
   }
---
+++ src/views/Index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li class="category" data-title="eeprom">
+      <ul>
+        <li class="category" data-title="tplink">
+          <ul>
+            <router-link to="/tplink/eeprom/wdr7300" custom v-slot="{ navigate }">
+              <li @click="navigate"><a href="/router/tplink/eeprom/wdr7300">wdr7300</a></li>
+            </router-link>
+          </ul>
+        </li>
+        <li class="category" data-title="zte">
+          <ul>
+            <router-link to="/zte/eeprom/e8820v2" custom v-slot="{ navigate }">
+              <li @click="navigate"><a href="/router/zte/eeprom/e8820v2">e8820v2</a></li>
+            </router-link>
+            <router-link to="/zte/eeprom/e8820s" custom v-slot="{ navigate }">
+              <li @click="navigate"><a href="/router/zte/eeprom/e8820s">e8820s</a></li>
+            </router-link>
+          </ul>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+<style lang="css" scoped>
+ul {
+  text-align: left;
+}
+.category {
+  position: relative;
+}
+.category::before {
+  content: attr(data-title);
+}
+</style>
